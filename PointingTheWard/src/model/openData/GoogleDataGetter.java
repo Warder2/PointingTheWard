b68b0dac45cab686a83590be0c52d522b23f83d2
SEONGBONG <SEONGBONG@seongbong>
2015-12-29 16:27:35 +0900

    AppTest 수정 GoolgleDataGetter추가

---
+++ PointingTheWard/src/model/openData/GoogleDataGetter.java
@@ -0,0 +1,34 @@
+package model.openData;
+
+
+import java.util.List;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import model.openData.template.DataGetterDOMTemplate;
+
+public class GoogleDataGetter extends DataGetterDOMTemplate{
+	@Override
+	protected <T> List<T> getData(Element root) {
+	
+		NodeList rows = root.getElementsByTagName("row");
+		
+		
+		for(int rowCnt = 0; rowCnt < rows.getLength(); rowCnt++){
+			Element row = (Element)rows.item(rowCnt);
+			NodeList elements = row.getElementsByTagName("element");
+			for(int elementCnt = 0 ; elementCnt < elements.getLength(); elementCnt++){
+				Element element = (Element) elements.item(elementCnt);
+				Element duration = (Element)element.getElementsByTagName("duration").item(0);
+				System.out.println("duration value : "+getValue("value",duration));
+				System.out.println("duration text : "+getValue("text",duration));
+				Element distance = (Element)element.getElementsByTagName("distance").item(0);
+				System.out.println("distance value : "+getValue("value",distance));
+				System.out.println("distance text : "+getValue("text",distance));
+			}
+		}
+		
+		return null;
+	}
+}
--- PointingTheWard/src/test/ApiTest.java
+++ PointingTheWard/src/test/ApiTest.java
@@ -1,28 +1,20 @@
 package test;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.http.HttpConnection;
 import org.junit.Test;
 
-
+import model.openData.GoogleDataGetter;
 import model.openData.RequestInfo;
 import model.openData.StoreZoneDataGetter;
 import model.openData.dataForm.StoreZoneDataForm;
 import model.openData.template.DataGetterTemplate;
 
 public class ApiTest {
-	//@Test
+	@Test
 	public void getStoreZone() throws IOException{
 		Map<String, String> parameters = new HashMap<String, String>();
 		parameters.put("type", "xml");
@@ -32,6 +24,7 @@ public class ApiTest {
 		
 		RequestInfo requestInfo = new RequestInfo("http://apis.data.go.kr/B553077/api/open/sdsc/storeZoneOne",
 				parameters, "ServiceKey", "b4%2FOQgFU8EroBosLvjRoPFNiXwHX8W2d8wFrsmJTm1r%2FeiOUcmlWXOzIU1j6%2BjsZgSumHK0TRaf87G%2BH%2BY1oIw%3D%3D");
+		
 		DataGetterTemplate templete = new StoreZoneDataGetter();
 		List<StoreZoneDataForm> infos = templete.getData(requestInfo);
 		for(StoreZoneDataForm dataForm : infos){
@@ -40,30 +33,20 @@ public class ApiTest {
 	}
 	
 	@Test
-	public void calendarTest() throws IOException{
-		StringBuffer url = new StringBuffer("https://www.googleapis.com/calendar/v3/calendars/janghaeye@gmail.com/events");
-		url.append("?" + URLEncoder.encode("key", "UTF-8") + "=AIzaSyAfCwtKwhFhdElxyvYLXweUWCm-9c_sCfw");
+	public void getGoogle(){
+		Map<String, String> parameters = new HashMap<String, String>();
+		parameters.put("origins", "Vancouver BC");		
+		parameters.put("destinations", "San Francisco");
+		parameters.put("mode", "transit");
+		parameters.put("language", "kr-ko");
+		RequestInfo requestInfo = new RequestInfo("https://maps.googleapis.com/maps/api/distancematrix/xml", parameters, "key", "AIzaSyB11fLFswQhh45Yh2a9UkBmHFIkAuTpniE");
 		
-		URL requestUrl = new URL(url.toString());
-		HttpURLConnection conn = (HttpURLConnection) requestUrl.openConnection();
-		conn.setRequestMethod("GET");
-		conn.setRequestProperty("Content-type", "application/json");
-		
-		System.out.println("response code : " + conn.getResponseCode());
-		if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300){
-			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String str = "";
-			while((str = reader.readLine()) != null){
-				System.out.println(str);
-			}
-			reader.close();
-		}else{
-			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-			String str = "";
-			while((str = reader.readLine()) != null){
-				System.out.println(str);
-			}
-			reader.close();
+		DataGetterTemplate templete = new GoogleDataGetter();
+		try {
+			templete.getData(requestInfo);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
