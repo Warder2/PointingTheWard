9696ca8cb2a8ec74d7fc91c95c73bb2b2cd74047
SEONGBONG <SEONGBONG@seongbong>
2015-12-31 04:24:55 +0900

    RecommendService class update

--- PointingTheWard/src/model/service/RecommendService.java
+++ PointingTheWard/src/model/service/RecommendService.java
@@ -1,5 +1,31 @@
 package model.service;
 
-public class RecommendService {
+import java.util.List;
 
+import model.ListVO.Calendar;
+import model.ListVO.PlaceList;
+import model.ListVO.WardList;
+
+public class RecommendService {
+	/**
+	 * 가능한 일정 조회
+	 * @return
+	 */
+	public Calendar possibleEventSearch(List<Calendar> calendar) {
+		return new Calendar();
+	}
+	/**
+	 * 가능한 일정에서 가능한 장소 조회
+	 * @return
+	 */
+	public PlaceList possiblePlaceSearch(Calendar calendar) {
+		return new PlaceList();
+	}
+	/**
+	 * 적합안 일정과 장소를 추천 
+	 * @return
+	 */
+	public WardList wardListRecommend(List<Calendar> calendar, PlaceList places){
+		return new WardList();
+	}
 }
