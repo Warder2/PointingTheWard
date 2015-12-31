b34629e6dfbcd8b4e47d918e73d6b6ca125369be
GatsbyDJ <devj47@gmail.com>
2016-01-01 04:19:07 +0900

    ServiceTest commit

--- PointingTheWard/src/test/ServiceTest.java
+++ PointingTheWard/src/test/ServiceTest.java
@@ -5,11 +5,9 @@ import org.junit.Test;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-import model.beans.Event;
 import model.beans.Member;
-import model.beans.Place;
-import model.beans.Point;
 import service.Service;
+import service.ServiceMacro;
 import service.ServiceRequest;
 
 public class ServiceTest {
@@ -36,26 +34,75 @@ public class ServiceTest {
 	//2
 	public void memberSignIn(){
 		System.out.println("memberSignIn");
+		
+		Service service = context.getBean("memberSignInService" , Service.class);
+		
+		// id,pw 유효성 체크
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("id", "admin");
+		serviceRequest.addObject("pw", "admin");
+		
+		service.execute(serviceRequest);
+		
 	}
 	//3
 	public void memberSearch(){
 		System.out.println("memberSearch");
+		
+		Service service = context.getBean("memberSearchService" , Service.class);
+		
+		
+		
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("email", "admin");
+		
+		service.execute(serviceRequest);
+		
 	}
 	//4
+
 	public void memberModify(){
 		System.out.println("memberModify");
+		Service service = context.getBean("memberModifyService" , Service.class);
+		
+		
+		
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("email", "admin");
+		serviceRequest.addObject("memberinfo", new Member("김대진","이메일","비번","주소","교통"));
+		service.execute(serviceRequest);
 	}
 	//5
+	@Test
 	public void memberDropout(){
 		System.out.println("memberDropout");
+		Service service = context.getBean("memberDropoutMacro" , Service.class);
+		
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("email", "admin");
+		service.execute(serviceRequest);
 	}
 	//6
 	public void eventRegist(){
 		System.out.println("eventRegist");
+		Service service = context.getBean("eventRegistService" , Service.class);
+		
+		
+		
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("id", "admin");
+		service.execute(serviceRequest);
 	}
 	//7
 	public void eventSearch(){
 		System.out.println("eventSearch");
+		Service service = context.getBean("memberSearchService" , Service.class);
+		
+		
+		
+		ServiceRequest serviceRequest = context.getBean("serviceRequest", ServiceRequest.class);
+		serviceRequest.addObject("id", "admin");
+		service.execute(serviceRequest);
 	}
 	//8
 	public void eventModify(){
@@ -131,7 +178,6 @@ public class ServiceTest {
 		System.out.println("wardRecommendService");
 	}
 	
-	
 	/*public void registEventAndDelete(){
 		System.out.println("registEventAndDelete");
 		service = context.getBean("registAndDeleteService", Service.class);
@@ -142,7 +188,4 @@ public class ServiceTest {
 		
 		service.execute(request);
 	}*/
-	
-	
-	
 }
