0879b04de96edccb74cd1134c57b03ab78e4c474
yejanghae <nj186@naver.com>
2016-01-02 00:32:43 +0900

    daoContext.xml 수정 juli 라이브러리 추가하는 코드 pom.xml에 추가

--- PointingTheWard/src/test/MemberTest.java
+++ PointingTheWard/src/test/MemberTest.java
@@ -1,22 +1,35 @@
 package test;
 
-import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.support.AbstractApplicationContext;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import model.beans.Transportation;
 import persistance.dao.MemberDAO;
+import persistance.dto.MemberDTO;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/daoContext.xml")
 public class MemberTest {
-	private AbstractApplicationContext context;
+	@Autowired
 	private MemberDAO memberDao;
+	private MemberDTO[] members;
 	
-	@After
+	@Before
 	public void setUp(){
-		context = new GenericXmlApplicationContext("daoContext.xml");
-		memberDao = context.getBean("memberDao", MemberDAO.class);
+		members = new MemberDTO[]{
+				new MemberDTO("email", "name", "pwd", "location", Transportation.TRANSIT),
+				new MemberDTO("email1", "name1", "pwd1", "location1", Transportation.TRANSIT),
+				new MemberDTO("email2", "name2", "pwd2", "location2", Transportation.DRIVING)
+		};
 	}
 	
 	@Test
-	public void signUp(){}
+	public void signUp(){
+		System.out.println(memberDao);
+	}
 }
